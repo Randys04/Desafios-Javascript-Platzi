7fe300421ec9db2ddb2e8f90559f5a994447259d
Randy Moya <stevenmoya02@gmail.com>
2023-07-20 11:50:27 -0600

    Se realizo una funcion la cual retorna el multiplicador en comun de un array recibido

--- ejercicios.js
+++ ejercicios.js
@@ -7,3 +7,34 @@ Ej: Si se reciben los numeros 18 y 2 la resolucion seria esta:
 export function solution(num1, num2) {
     return parseInt(`${num1 - num2}${num1 + num2}`)
 }
+
+/*
+Solucion a ejercicio en que recibimos un array de numeros y se retorna el numero por el
+que si multiplicamos del 3 al 8 obtenemos los resultados del array. Em caso de algun
+resultado posea un multiplicador diferente a los demas resutados del array se retorna false
+Ej: si recibimos el array [27, 36, 45, 54, 63, 72] el valor retornado seria 9 ya que:
+    3 * 9 =  27
+    4 * 9 =  36
+    5 * 9 =  45
+    6 * 9 =  54
+    7 * 9 =  63
+    8 * 9 =  72
+En cambio si recibimos el array [27, 36, 45, 54, 42, 72] el valor retornado es false ya que:
+    3 * 9 =  27
+    4 * 9 =  36
+    5 * 9 =  45
+    6 * 9 =  54
+    7 * 6 =  42 *el multiplicador es diferente al de los demas resultados 
+    8 * 9 =  72
+*/ 
+export function solution(numbers) {
+
+    let multiplicador = numbers[0] / 3;
+
+    for (let i = 4; i <= 8; i++) {
+        if (numbers[i -3 ] / i != multiplicador) {
+            return false;
+        }
+    }
+    return multiplicador;
+}
